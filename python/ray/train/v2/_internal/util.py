b24292454a9b7d9173a087fa5a698743fb0113d1
Justin Yu <justinvyu@anyscale.com>
2026-02-13 12:01:45 -0800

    handle object_store_memory field in remote args
    Signed-off-by: Justin Yu <justinvyu@anyscale.com>

--- python/ray/train/v2/_internal/util.py
+++ python/ray/train/v2/_internal/util.py
@@ -31,14 +31,15 @@ T = TypeVar("T")
 def bundle_to_remote_args(bundle: dict) -> dict:
     """Convert a bundle of resources to Ray actor/task arguments.
 
-    >>> bundle_to_remote_args({"GPU": 1, "memory": 1, "custom": 0.1})
-    {'num_cpus': 0, 'num_gpus': 1, 'memory': 1, 'resources': {'custom': 0.1}}
+    >>> bundle_to_remote_args({"GPU": 1, "memory": 1, "object_store_memory": 1024, "custom": 0.1})
+    {'num_cpus': 0, 'num_gpus': 1, 'memory': 1, 'object_store_memory': 1024, 'resources': {'custom': 0.1}}
     """
     bundle = bundle.copy()
     args = {
         "num_cpus": bundle.pop("CPU", 0),
         "num_gpus": bundle.pop("GPU", 0),
         "memory": bundle.pop("memory", 0),
+        "object_store_memory": bundle.pop("object_store_memory", 0),
     }
     if bundle:
         args["resources"] = bundle
